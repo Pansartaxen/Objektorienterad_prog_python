0b178c970cbea0e2090cbf51c514bd2798764b4b
Marius Stokkedal <matk20@student.bth.se>
2021-10-18 08:27:27 +0200

    Tog bort kommentarer och sparade dem i separat fil

--- Backup.py
+++ Backup.py
@@ -11,6 +11,8 @@ class Diner:
         self._pickup_worker = inRoad
         self._pickup_food = inBarn
         self._dropoff_worker = outRoad
+        #self._has_worker = False
+        #self._has_food = False
         self._worker = []
         self._food = []
         self._prio = True
@@ -47,9 +49,15 @@ class Diner:
 
 class Barn:
     def __init__(self):
+        #self._queue = [Food() for _ in range(100)]
         self._queue = []
     
     def add_food(self):
+        # x = random.randint(1,1000)
+        # if x > 600:
+        #     for _ in range(2):
+        #         self._queue.append(Food())
+        # else:
         self._queue.append(Food())
 
     def remove_food(self):
@@ -73,7 +81,8 @@ class Field:
         self._pickup_worker = inRoad
         self._dropoff_food = outBarn
         self._dropoff_worker = outRoad
-        self._worker = []
+        #self._has_worker = False
+        self._worker = [] #Ny
         self._accident = Accident_generator()
 
     def set_in_worker(self, inRoad):
@@ -90,6 +99,7 @@ class Field:
 
     def action(self):
         self._worker.append(self._pickup_worker.remove_worker())
+        #if self._has_worker:
         if self._worker[0] != None:
             if self._accident.accident_check():
                 hp = -0.75 * self._worker[0].health()
@@ -106,7 +116,7 @@ class House:
         self._pickup_product = inStorage
         self._dropoff_worker = outRoad
         self._create_worker = True
-        self._worker = []
+        self._worker = [] #Ny
         self._product = []
 
     def set_in_worker(self, inRoad):
@@ -125,7 +135,7 @@ class House:
         self._create_worker = create
 
     def action(self):
-        if self._create_worker == True and self._pickup_worker.que_length() >= 2:
+        if self._create_worker == True and self._pickup_worker.que_length() >= 2: #self._worker[1] != None:
             self._worker.append(self._pickup_worker.remove_worker())
             self._worker.append(self._pickup_worker.remove_worker())
             self._product.append(self._pickup_product.remove_product())
@@ -133,8 +143,9 @@ class House:
                 self._dropoff_worker.add_worker(self._worker[i])
             self._worker = []
             for _ in range(random.randint(1,2)):
-                self._dropoff_worker.add_worker(Worker())
-        elif self._pickup_worker.que_length() == 1:
+                self._dropoff_worker.add_worker(Worker()) #Creates new worker
+        #elif self._worker[0] != None:
+        elif self._pickup_worker.que_length() == 1:#self._worker[0] != None:
             self._worker.append(self._pickup_worker.remove_worker())
             self._product.append(self._pickup_product.remove_product())
             self._worker[0].change_hp(50)
@@ -147,6 +158,7 @@ class House:
 class Road:
     def __init__(self):
         self._queue = [Worker() for _ in range(2)]
+        #self._queue = []
 
     def remove_worker(self):
         try:
@@ -159,6 +171,7 @@ class Road:
         y == int((len(self._queue))//10)
         if x == 4:
             self._queue = self._queue[0:5]
+            #print('x-x-x-x-x-x-x-x-x-x-x-x-x-x-x')
 
 
     def add_worker(self, worker):
@@ -188,7 +201,8 @@ class Factory:
         self._pickup_worker = inRoad
         self._dropoff_product = outStorage
         self._dropoff_worker = outRoad
-        self._worker = []
+        #self._has_worker = False
+        self._worker = [] #Ny
         self._accident = Accident_generator()
         self._damage = random.randint(-30,-10)
 
@@ -205,24 +219,35 @@ class Factory:
         return([self._pickup_worker,self._dropoff_product,self._dropoff_worker])
 
     def action(self):
+        #if self._has_worker:
         self._worker.append(self._pickup_worker.remove_worker())
         if self._worker[0] != None:
             if self._accident.accident_check():
                 hp = -1 * self._worker[0].health()
-                self._worker[0].change_hp(hp)
+                self._worker[0].change_hp(hp) #Kills worker
+                #print('Worker died. #Sad')
                 self._worker.pop(0)
+                #self._has_worker = False
             else:
+                #hp = -0.75 * self._worker[0].health()
                 self._worker[0].change_hp(self._damage)
+                #self._worker[0].change_hp(-500)
                 self._dropoff_worker.add_worker(self._worker[0])
                 for _ in range(random.randint(1,3)):
                     self._dropoff_product.add_product(Product())
                 self._worker.pop(0)
+                #self._has_worker = False
 
 class Storage:
     def __init__(self):
+        #self._queue = [Product() for _ in range(100)]
         self._queue = []
 
     def add_product(self, product):
+        # x = random.randint(500,1000)
+        # if x > 400:
+        #     for _ in range(2):
+        #         self._queue.append(product)
         self._queue.append(product)
 
 
@@ -239,6 +264,11 @@ class Product:
     def __init__(self):
         pass
 
+class Government:
+    def __init__(self):
+        self._goal_road = 100
+        self._goal_barn = 100
+        self._goal_storage = 100
 
 if __name__ == "__main__":
     r1 = Road()
@@ -249,6 +279,13 @@ if __name__ == "__main__":
     b1 = Barn()
     b2 = Barn()
 
+    # for _ in range(10000):
+    #     r1.add_worker(Worker())
+    #     r2.add_worker(Worker())
+    #     s1.add_product(Product())
+    #     b1.add_food()
+    #     b2.add_food()
+    
     roads = [r1,r2,r3]
     storages = [s1,s2]
     barns = [b1,b2]
@@ -265,11 +302,22 @@ if __name__ == "__main__":
     h3 = House(r2,s1,r2)
     h4 = House(r2,s1,r1)
 
+    #transitions = [f1,h1,f2,d1,d2,h2,d3,fiel1,h3,fiel2,h4]
     transitions = []
 
     count = 0
+    # while r1.que_length() > 0 or r2.que_length() > 0:
+    #     print(f'r1:{r1.que_length()}, r2:{r2.que_length()}, s1:{s1.que_length()}, b1:{b1.que_length()}, b2:{b2.que_length()}')
+    #     for i in transitions:
+    #         i.action()
+    #         count += 1
+    #     print('---')
+    # print(f'-------- count: {count} -------')
+    # print(f'r1:{r1.que_length()}, r2:{r2.que_length()}, s1:{s1.que_length()}, b1:{b1.que_length()}, b2:{b2.que_length()}')
+    #run = True
 
-    while r1.que_length() > 0 or r2.que_length() > 0:
+    while r1.que_length() > 0 or r2.que_length() > 0: #and run == True:
+        #input('x-x-x-x-x-x-x-x-x-x-x-x Tryck enter förr att köra 200 rundor x-x-x-x-x-x-x-x-x-x-x-x')
         time.sleep(0.05)
         for _ in range(1):
             for t in transitions:
@@ -315,6 +363,11 @@ if __name__ == "__main__":
             for i in roads:
                 if i.que_length() <= 100:
                     transitions.append(House(i,maxStorage,i))
+                    # for x in transitions:
+                    #         if type(x).__name__ == 'House':
+                    #             if x.connections()[2] == i:
+                    #                 x.change_priority(True)
+                            #transitions.append(House(i,maxStorage,i))
                 elif i.que_length() > 100:
                     for x in transitions:
                         if type(x).__name__ == 'House':
@@ -322,7 +375,10 @@ if __name__ == "__main__":
                                 index = transitions.index(x)
                                 transitions.pop(index)
                                 break
-
+                            #break
+                        #break
+                                #x.change_priority(False)
+            
             for i in barns:
                 if i.que_length() <= 25:
                     transitions.append(Field(maxRoad,i,minRoad))
@@ -331,6 +387,7 @@ if __name__ == "__main__":
                                 if x.connections()[1] == i:
                                     index = transitions.index(x)
                                     transitions.pop(index)
+                                    #x.change_priority(False)
                 elif i.que_length() > 25:
                     transitions.append(Diner(minRoad,i,minRoad))
                     for x in transitions:
@@ -338,6 +395,7 @@ if __name__ == "__main__":
                                 if x.connections()[1] == i:
                                     index = transitions.index(x)
                                     transitions.pop(index)
+                                    #x.change_priority(True)
 
             for i in storages:
                 if i.que_length() <= 25:
@@ -352,9 +410,13 @@ if __name__ == "__main__":
 
             x = []
             output = f'||    Rundor: {count:<4}    ||'
+            #print(f'-------- Rundor: {count} -------')
+            #print(f'-----------------------------------------------------------transitioner--{len(transitions)}')
             output += f'    transitioner: {len(transitions):<4}'
             for i in transitions:
                 x.append(type(i).__name__)
+            #print('----------------------------------------------------------------------------------------',set(x))
+            #output += f'--{set(x)}'
             y = ''
             output +=f' ||   r1:{r1.que_length():<3} r2:{r2.que_length():<3} r3:{r3.que_length():<3} s1:{s1.que_length():<3} s2:{s2.que_length():<3} b1:{b1.que_length():<3} b2:{b2.que_length():<2}'
             output += f'{y:<4}||    {set(x)}'
